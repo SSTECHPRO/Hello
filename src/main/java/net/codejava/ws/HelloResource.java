d3adb799dea9d4f3dfbf8421d4d5badcd0fc79ea
SSTECHPRO <81318213+SSTECHPRO@users.noreply.github.com>
2021-05-30 11:34:45 +0530

    Initial commit

---
+++ src/main/java/net/codejava/ws/HelloResource.java
@@ -0,0 +1,23 @@
+package net.codejava.ws;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+ 
+@Path("/bonjour")
+public class HelloResource {
+ 
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String direBonjour() {
+        return "Bonjour, tout le monde!";
+    }    
+        
+        
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student sayJsonHello() {
+    	return new Student(123, "SANJAY", "India");
+    }
+}
